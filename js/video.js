db14f1879611a6abf70b385534320b4524934a2d
this-sangyeon <92290951+this-sangyeon@users.noreply.github.com>
2022-01-07 21:00:05 +0900

    arrange

--- js/video.js
+++ js/video.js
@@ -5,13 +5,10 @@ const prevBtn = document.querySelector('.prev-button');
 const nextBtn = document.querySelector('.next-button');
 const videoRespon = document.querySelector('.video-respon-wrap > video');
 const videoText = document.querySelector('.video-respon-wrap > .video-text > p');
-
-
 var count = 0;
 let videoLength = videoListEls.length;
 let currentIndex = 0;
 let videoListIndex = 0;
-
 let videoLink =[
     {
         video:'./img/media/video/TimeofOurLifeMV.mp4',
@@ -35,105 +32,11 @@ let videoLink =[
     }
 ]
 let videoListLength = videoLink.length;
-// function deskTopResize(){
-//     prevBtn.addEventListener('click', ()=>{
-//         if(matchMedia("screen and (max-width : 1920px) and (max-height: 1080px)").matches){
-//             console.log('클릭 눌리는지요');
-//             count--; 
-//             console.log(count);
-//             // transform = `rotate( ${-72 * count}deg)`;
-//             videoListEl.style.transform = `rotate( ${72 * count * -1}deg)`;
-//             videoListEl.style.transition="0.7s";     
-        
-//             for(let i = 0; i < videoListEls.length; i++){
-//                 videoListEls[i].style.opacity = 0;
-//             }
-//             if(count > 0){
-//                 if(count >= videoListEls.length){
-//                     videoListEls[count % videoListEls.length].style.opacity = 1;
-//                 } else {
-//                     videoListEls[count].style.opacity = 1;
-//                 }
-//             } else if(count <= 0){ 
-//                 if(Math.abs(count) >= videoListEls.length){
-//                     if(Math.abs(count) % videoListEls.length == 0) {
-//                         videoListEls[0].style.opacity = 1;
-//                     }else{ 
-//                         videoListEls[videoListEls.length - Math.abs(count) % videoListEls.length].style.opacity = 1;
-//                     }
-//                 } else {
-//                     Math.abs(count) === 0 ? videoListEls[0].style.opacity = 1 : 
-//                     videoListEls[videoListEls.length - Math.abs(count)].style.opacity = 1;
-//                 }
-//             }
-//         }
-//     })
-//     nextBtn.addEventListener('click',()=>{
-//         console.log('클릭 눌리는지요');
-//         count++;
-//         console.log(count);
-//         videoListEl.style.transform = `rotate( ${72 * count * -1}deg)`; 
-//         videoListEl.style.transition="0.7s";
-    
-//         for(let i = 0; i < videoListEls.length; i++){
-//             videoListEls[i].style.opacity = 0;
-//         }
-//         if(count >= 0){
-//             if(count >= videoListEls.length){
-//                 count % videoListEls.length === 0 ? videoListEls[0].style.opacity = 1 : 
-//                 videoListEls[count % videoListEls.length].style.opacity = 1;
-//             }else {
-//                 videoListEls[count].style.opacity = 1;
-//             }
-//         } else if(count < 0){
-//             if(Math.abs(count) > videoListEls.length){
-//                 Math.abs(count) % videoListEls.length == 0 ? videoListEls[0].style.opacity = 1 : 
-//                 videoListEls[videoListEls.length - Math.abs(count) % videoListEls.length].style.opacity = 1;
-//             } else {
-//                 videoListEls[videoListEls.length - Math.abs(count)].style.opacity = 1;
-//             }
-//         } 
-//     })
-// }
-    
-// function tabletResize(){
-//     prevBtn.addEventListener('click',()=>{
-//         if(matchMedia("screen and (max-width : 1024px) and (max-height: 1366px)").matches){
-//             console.log('prevclick');
-//             currentIndex = currentIndex - 1;
-//             currentIndex < 0 && (currentIndex = videoLength - 1);
-//             console.log(currentIndex);
-//             for(let i = 0; i < videoListEls.length; i++){
-//                 videoListEls[i].classList.remove('active');
-//             }
-//             videoListEls[currentIndex].classList.add('active');
-//         }
-//     })
-//     nextBtn.addEventListener('click', ()=>{
-//         if(matchMedia("screen and (max-width : 1024px) and (max-height: 1366px)").matches){
-//             currentIndex = currentIndex + 1;
-//             currentIndex > videoLength - 1 && (currentIndex = 0);
-//             console.log(currentIndex);
-//             for(let i = 0; i < videoListEls.length; i++){
-//                 videoListEls[i].classList.remove('active');
-//             }
-//             videoListEls[currentIndex].classList.add('active');
-//         }
-//     })
-// }
-
-// window.addEventListener('resize',()=>{
-//     tabletResize();
-//     deskTopResize();
-// })
-
 
 prevBtn.addEventListener('click', ()=>{
     if(matchMedia("screen and (max-width : 1920px) and (max-height: 1080px)").matches){
-        console.log('클릭 눌리는지요');
         count--; 
         console.log(count);
-        // transform = `rotate( ${-72 * count}deg)`;
         videoListEl.style.transform = `rotate( ${72 * count * -1}deg)`;
         videoListEl.style.transition="0.7s";     
     
@@ -173,16 +76,12 @@ prevBtn.addEventListener('click', ()=>{
         videoText.innerText = videoLink[videoListIndex].text;
     }
 })
-// const videoRespon = document.querySelector('.video-respon-wrap > video');
-// const videoText = document.querySelector('.video-respon-wrap > p');
 nextBtn.addEventListener('click', ()=>{
     if(matchMedia("screen and (max-width : 1920px) and (max-height: 1080px)").matches){
-        console.log('클릭 눌리는지요');
         count++;
         console.log(count);
         videoListEl.style.transform = `rotate( ${72 * count * -1}deg)`; 
         videoListEl.style.transition="0.7s";
-    
         for(let i = 0; i < videoListEls.length; i++){
             videoListEls[i].style.opacity = 0;
         }
@@ -193,7 +92,7 @@ nextBtn.addEventListener('click', ()=>{
             }else {
                 videoListEls[count].style.opacity = 1;
             }
-        } else if(count < 0){
+        }else if(count < 0){
             if(Math.abs(count) > videoListEls.length){
                 Math.abs(count) % videoListEls.length == 0 ? videoListEls[0].style.opacity = 1 : 
                 videoListEls[videoListEls.length - Math.abs(count) % videoListEls.length].style.opacity = 1;
@@ -215,106 +114,6 @@ nextBtn.addEventListener('click', ()=>{
         videoListEls[videoListIndex].classList.add('active');
     }
 })
-
-
-
-
-
-// if(matchMedia("screen and (max-width : 1920px) and (max-height: 1080px)").matches){
-//     prevBtn.addEventListener('click', function(){
-//         console.log('클릭 눌리는지요');
-//         count--; 
-//         console.log(count);
-//         // transform = `rotate( ${-72 * count}deg)`;
-//         videoListEl.style.transform = `rotate( ${72 * count * -1}deg)`;
-//         videoListEl.style.transition="0.7s";     
-    
-//         for(let i = 0; i < videoListEls.length; i++){
-//             videoListEls[i].style.opacity = 0;
-//         }
-//         if(count > 0){
-//             // 카운트가 비디오 개수보다 크거나 같다면 
-//             //( count >= 5 )
-//             if(count >= videoListEls.length){
-//                 videoListEls[count % videoListEls.length].style.opacity = 1;
-//             } else {
-//                 videoListEls[count].style.opacity = 1;
-//             }
-//         } else if(count <= 0){ 
-//             if(Math.abs(count) >= videoListEls.length){
-//                 if(Math.abs(count) % videoListEls.length == 0) {
-//                     videoListEls[0].style.opacity = 1;
-//                 }else{ 
-//                     videoListEls[videoListEls.length - Math.abs(count) % videoListEls.length].style.opacity = 1;
-//                 }
-//             } else {
-//                 Math.abs(count) === 0 ? videoListEls[0].style.opacity = 1 : 
-//                 videoListEls[videoListEls.length - Math.abs(count)].style.opacity = 1;
-//             }
-//         }
-//     })
-//     // 동일한 방법으로 식 진행
-//     nextBtn.addEventListener('click', function(){
-//         console.log('클릭 눌리는지요');
-//         count++;
-//         console.log(count);
-//         videoListEl.style.transform = `rotate( ${72 * count * -1}deg)`; 
-//         videoListEl.style.transition="0.7s";
-    
-//         for(let i = 0; i < videoListEls.length; i++){
-//             videoListEls[i].style.opacity = 0;
-//         }
-//         if(count >= 0){
-//             if(count >= videoListEls.length){
-//                 count % videoListEls.length === 0 ? videoListEls[0].style.opacity = 1 : 
-//                 videoListEls[count % videoListEls.length].style.opacity = 1;
-//             }else {
-//                 videoListEls[count].style.opacity = 1;
-//             }
-//         } else if(count < 0){
-//             if(Math.abs(count) > videoListEls.length){
-//                 Math.abs(count) % videoListEls.length == 0 ? videoListEls[0].style.opacity = 1 : 
-//                 videoListEls[videoListEls.length - Math.abs(count) % videoListEls.length].style.opacity = 1;
-//             } else {
-//                 videoListEls[videoListEls.length - Math.abs(count)].style.opacity = 1;
-//             }
-//         }       
-//     })
-// }
-// nextBtn.addEventListener('click',()=>{
-
-// })
-// else if(matchMedia("screen and (max-width : 1024px) and (max-height: 1366px)").matches){
-//     prevBtn.addEventListener('click',()=>{
-//         console.log('prevclick');
-//         currentIndex = currentIndex - 1;
-//         currentIndex < 0 && (currentIndex = videoLength - 1);
-//         console.log(currentIndex);
-//         for(let i = 0; i < videoListEls.length; i++){
-//             videoListEls[i].classList.remove('active');
-//         }
-//         videoListEls[currentIndex].classList.add('active');
-//     })
-//     nextBtn.addEventListener('click',()=>{
-        
-//         console.log('prevclick');
-//         currentIndex = currentIndex + 1;
-//         currentIndex > videoLength - 1 && (currentIndex = 0);
-//         console.log(currentIndex);
-//         for(let i = 0; i < videoListEls.length; i++){
-//             videoListEls[i].classList.remove('active');
-//         }
-//         videoListEls[currentIndex].classList.add('active');
-//     })
-// }
-
-// window.onresize = function(){ 
-//     window.location.reload(); 
-// };
-
-// section[3].addEventListener('resize',()=>{
-//     console.log('ddd');
-// })
 
     
 
